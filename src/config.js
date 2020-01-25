71e2d80d18a0ebf478fce93f57704f0dd264aacd
Christian Gioia <christiang101@gmail.com>
2020-01-25 15:05:38 +1100

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-cgioia"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://p8thhf45jh.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_0uJaDkRU5",
+    APP_CLIENT_ID: "1vm307iro0q99b42qoc5ehtn3l",
+    IDENTITY_POOL_ID: "us-east-2:541455be-a6eb-4307-a2da-a143c52664c0 "
+  }
+};
